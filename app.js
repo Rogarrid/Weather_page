1b7eb0ef79dc4b4a49b94bc714a8d711901d38de
Rocio <111855553+Rogarrid@users.noreply.github.com>
2023-02-14 13:16:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 	const city = req.body.city;
-	const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=3d905385c00a581723fe8ddde72c3964&units=metric";
+	const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=APIKEY&units=metric";
 
 	https.get(url, function(response){
 		response.on("data", function(data){
